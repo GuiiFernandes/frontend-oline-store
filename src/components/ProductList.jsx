ecb0ec073218bb2e44707c990da8343d678b295a
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-05-22 17:36:45 -0300

    feat(requisito 5): imlementa ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import ProductCard from './ProductCard';
 
 export default class ProductList extends Component {
   render() {
+    const { query } = this.props;
+
     return (
-      <div>ProductList</div>
+      <div>
+        {
+          query.map((product) => (
+            <ProductCard
+              key={ product.id }
+              product={ product }
+            />
+          ))
+        }
+      </div>
     );
   }
 }
+
+ProductList.propTypes = {
+  query: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  })).isRequired,
+};
