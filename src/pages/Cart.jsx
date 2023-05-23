a597ab073af5e3f2e9023681ee3a5de703714420
GuiiFernandes <guifjj92@gmail.com>
2023-05-23 18:59:24 -0300

    feat(Requisito 10) Criei incremento e decremento no carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { changeQuantity, removeProduct } from '../services/localStorage';
 
 export default class Cart extends Component {
   state = {
@@ -12,8 +13,21 @@ export default class Cart extends Component {
     });
   }
 
+  handleChangeQuantity = (product, mult) => {
+    this.setState({
+      productsInCart: changeQuantity(product, mult),
+    });
+  };
+
+  handleRemoveProduct = (product) => {
+    this.setState(({ productsInCart }) => ({
+      productsInCart: removeProduct(product, productsInCart),
+    }));
+  };
+
   render() {
     const { productsInCart } = this.state;
+    const negativeMult = -1;
     return (
       <main>
         <nav>
@@ -23,17 +37,35 @@ export default class Cart extends Component {
         </nav>
         <section>
           { productsInCart.length
-            ? productsInCart.map(({ title, thumbnail, price, id, quantity }) => (
-              <div key={ id }>
-                <span>remover</span>
-                <img src={ thumbnail } alt={ title } />
-                <span data-testid="shopping-cart-product-name">{ title }</span>
+            ? productsInCart.map((product) => (
+              <div key={ product.id }>
+                <button
+                  data-testid="remove-product"
+                  onClick={ () => this.handleRemoveProduct(product) }
+                >
+                  remover
+                </button>
+                <img src={ product.thumbnail } alt={ product.title } />
+                <span data-testid="shopping-cart-product-name">{ product.title }</span>
                 <span data-testid="shopping-cart-product-quantity">
+                  <button
+                    data-testid="product-decrease-quantity"
+                    onClick={ () => this.handleChangeQuantity(product, negativeMult) }
+                    disabled={ product.quantity === 1 }
+                  >
+                    -
+                  </button>
                   <strong>
-                    { quantity }
+                    { product.quantity }
                   </strong>
+                  <button
+                    data-testid="product-increase-quantity"
+                    onClick={ () => this.handleChangeQuantity(product, 1) }
+                  >
+                    +
+                  </button>
                 </span>
-                <span>{ price }</span>
+                <span>{ product.price }</span>
               </div>
             )) : (
               <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
